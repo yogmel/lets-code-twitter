73170e2f2c107e07ddf0809ec655870c776fc33b
Mellina Y <yonashiro.mellina@gmail.com>
2021-09-10 18:06:53 -0300

    Fix warnings

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,13 +2,13 @@ import { Route, Redirect } from "react-router-dom";
 import { LoginContainer as Login } from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import { Tweets } from "./Tweets";
-import { useState, useEffect } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { watchAllTweets } from "./../db/firebaseConfig";
 import { CurrentUserContext, TweetsContext } from "./../context";
 
 function Routes() {
   const [tweets, setTweets] = useState([]);
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   const storeTweets = (tweetsObj) => {
     const tweets = [];
@@ -18,17 +18,17 @@ function Routes() {
     setTweets(tweets);
   };
 
-  const fetchTweets = async () => {
+  const fetchTweets = useCallback(async () => {
     watchAllTweets(storeTweets);
-  };
+  }, []);
 
   useEffect(() => {
     fetchTweets();
-  }, []);
+  }, [fetchTweets]);
 
   return (
     <TweetsContext.Provider value={tweets}>
-      <CurrentUserContext.Provider value={user}>
+      <CurrentUserContext.Provider value={null}>
         <PrivateRoute path="/geral">
           <Tweets visibility="all" />
         </PrivateRoute>
